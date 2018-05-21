3591fdfae771505ddfc47904c52b7812e7c86425
tryexceptelse <schwarzjohnv@gmail.com>
2018-05-21 11:55:58 -0700

    Added some documentation to model.py

--- wave/model.py
+++ wave/model.py
@@ -1,3 +1,8 @@
+"""
+Module containing the visualization program and model for accessing and
+modifying its parameters.
+"""
+
 import math
 import vispy.gloo as gloo
 
@@ -15,16 +20,16 @@ FRAG_SHADER = """
 
 #define M_PI 3.1415926535897932384626433832795
 
-#define C 60.0
+#define C 60.0  // Rate of wave propagation in visualization.
 
 uniform float u_global_time;
 
-uniform float u_lambda = 4.0;
+uniform float u_lambda = 4.0;       // Wavelength
 uniform float u_contrast = 0.25;
 
-uniform float u_phase[N_SOURCES];
-uniform float u_amp[N_SOURCES];
-uniform float u_dir[N_SOURCES];
+uniform float u_phase[N_SOURCES];   // Source phases, in radians.
+uniform float u_amp[N_SOURCES];     // Source amplitudes.
+uniform float u_dir[N_SOURCES];     // Source directions, in radians.
 
 /**
  * Finds value of wave at passed position.
@@ -40,6 +45,11 @@ float f(vec2 uv, int src) {
     return v;
 }
 
+/**
+ * Finds maximum possible amplitude, if all waves were to 
+ * mix constructively.
+ * Used to determine displayed brightness value.
+ */
 float find_max_amp() {
     float r = 0.0;
     for (int i=0; i < N_SOURCES; ++i) {
@@ -48,6 +58,10 @@ float find_max_amp() {
     return r;
 }
 
+/**
+ * Converts passed actual value and maximum value into an rgb value
+ * to be displayed.
+ */
 vec3 value_to_rgb(float v, float max) {
     if (max == 0.0) {
         return vec3(0.0, 0.0, 0.0);
@@ -64,17 +78,21 @@ void main(void) {
     }
     gl_FragColor = vec4(value_to_rgb(v, max), 1.000);
 }
-""".replace('N_SOURCES', str(N_SOURCES))  # format breaks the many brackets.
+""".replace('N_SOURCES', str(N_SOURCES))  # .format() breaks the many brackets.
 
 
 class Model:
+    """
+    Stores OpenGL program and other information, along with means
+    to manipulate simulated emitter sources.
+    """
     def __init__(self):
         self.program = gloo.Program(VERT_SHADER, FRAG_SHADER)
         self.program['u_global_time'] = 0
         self.program['a_position'] = [(-1, -1), (-1, +1),
                                       (+1, -1), (+1, +1)]
-        self.contrast = 0.5  # use setter to change uniform
-        self.wavelength = 4  # use setter to change uniform
+        self.contrast = 0.5  # Uses setter to change uniform.
+        self.wavelength = 4  # Uses setter to change uniform.
 
         self.sources = [SourceControl(self.program, i)
                         for i in range(N_SOURCES)]
@@ -103,6 +121,7 @@ class Model:
 
 
 class SourceControl:
+    """ Controls parameters of an emitter in the OpenGL program. """
     def __init__(self, program: gloo.Program, i: int):
         self.program = program
         self.i = i
