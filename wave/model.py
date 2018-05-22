a00d8a14e38209a21a525432b7a83f80c51db485
tryexceptelse <schwarzjohnv@gmail.com>
2018-05-22 15:02:36 -0700

    Fixed bugs, added instantaneous phase mode.

--- wave/model.py
+++ wave/model.py
@@ -10,8 +10,10 @@ import vispy.gloo as gloo
 N_SOURCES = 4
 
 MODES = collections.OrderedDict((
-    ('wave_function', 0),
-    ('amplitude', 1)
+    ('wave function', 0),
+    ('amplitude', 1),
+    ('relative phase', 2),
+    ('instantaneous phase', 3),
 ))
 
 VERT_SHADER = """
@@ -30,6 +32,8 @@ FRAG_SHADER = """
 
 #define WAVE_FUNCTION 0
 #define AMPLITUDE 1
+#define RELATIVE_PHASE 2
+#define INSTANTANEOUS_PHASE 3
 
 uniform float u_global_time;
 uniform int u_mode = 0;
@@ -59,9 +63,21 @@ float f(vec2 uv, int src) {
  * Finds phasor of a specified wave source at a specified position.
  */
 vec2 find_phasor(vec2 uv, int src) {
-    float relative_phase = u_dir[src];
-    float x = uv.x * cos(relative_phase) - uv.y * sin(relative_phase);
-    float phase = relative_phase + x / u_lambda;
+    float dir = u_dir[src];
+    float x = uv.x * cos(dir) - uv.y * sin(dir);
+    float phase = u_phase[src] + x / u_lambda;
+    float amp = u_amp[src];
+    return vec2(sin(phase) * amp, cos(phase) * amp);
+}
+
+/**
+ * Finds phasor of a specified wave source at a specified position.
+ */
+vec2 find_instant_phasor(vec2 uv, int src) {
+    float dir = u_dir[src];
+    float x = uv.x * cos(dir) - uv.y * sin(dir);
+    float phase = u_phase[src] + x / u_lambda + 
+        mod(u_global_time * C, 2 * u_lambda * M_PI) / u_lambda;
     float amp = u_amp[src];
     return vec2(sin(phase) * amp, cos(phase) * amp);
 }
@@ -91,6 +107,73 @@ vec3 value_to_rgb(float v, float max) {
     return vec3(display_v, display_v, display_v);
 }
 
+vec3 hsv2rgb(vec3 hsv) {
+    float      hh, p, q, t, ff;
+    int        i;
+    vec3       rgb;
+
+    if(hsv.s <= 0.0) {
+        rgb.x = hsv.x;
+        rgb.y = hsv.x;
+        rgb.z = hsv.x;
+        return rgb;
+    }
+    hh = hsv.x;
+    if(hh >= 360.0) hh = 0.0;
+    hh /= 60.0;
+    i = int(hh);
+    ff = hh - i;
+    p = hsv.z * (1.0 - hsv.y);
+    q = hsv.z * (1.0 - (hsv.y * ff));
+    t = hsv.z * (1.0 - (hsv.y * (1.0 - ff)));
+
+    switch(i) {
+        case 0:
+            rgb.x = hsv.z;
+            rgb.y = t;
+            rgb.z = p;
+            break;
+        case 1:
+            rgb.x = q;
+            rgb.y = hsv.z;
+            rgb.z = p;
+            break;
+        case 2:
+            rgb.x = p;
+            rgb.y = hsv.z;
+            rgb.z = t;
+            break;
+        case 3:
+            rgb.x = p;
+            rgb.y = q;
+            rgb.z = hsv.z;
+            break;
+        case 4:
+            rgb.x = t;
+            rgb.y = p;
+            rgb.z = hsv.z;
+            break;
+        case 5:
+        default:
+            rgb.x = hsv.z;
+            rgb.y = p;
+            rgb.z = q;
+            break;
+    }
+    return rgb;
+}
+
+/**
+ * Converts a passed angle into an rgb value.
+ */
+vec3 theta2rgb(float theta) {
+    if (theta < 0.0) {
+        theta += M_PI * 2.0;
+    }
+    vec3 hsv = vec3(theta / M_PI * 180.0, u_contrast, u_contrast);
+    return hsv2rgb(hsv);
+}
+
 /**
  * Finds sum of all wave function results.
  */
@@ -113,19 +196,57 @@ float amplitude_sum(vec2 uv) {
     return length(phasor_sum);
 }
 
+/**
+ * Finds amplitude sum at a given position.
+ */
+float phase_sum(vec2 uv) {
+    vec2 phasor_sum = vec2(0.0, 0.0);
+    for (int i = 0; i < N_SOURCES; ++i) {
+        phasor_sum += find_phasor(uv, i);
+    }
+    float theta = atan(phasor_sum.y / phasor_sum.x);
+    if (phasor_sum.x < 0.0) {
+        theta += M_PI;
+    }
+    return theta;
+}
+
+/**
+ * Finds amplitude sum at a given position.
+ */
+float instant_phase_sum(vec2 uv) {
+    vec2 phasor_sum = vec2(0.0, 0.0);
+    for (int i = 0; i < N_SOURCES; ++i) {
+        phasor_sum += find_instant_phasor(uv, i);
+    }
+    float theta = atan(phasor_sum.y / phasor_sum.x);
+    if (phasor_sum.x < 0.0) {
+        theta += M_PI;
+    }
+    return theta;
+}
+
 void main(void) {
-    float max = find_max_amp();
-    float v;
+    float max, v;
+    vec3 rgb;
     vec2 uv = gl_FragCoord.xy;
     switch(u_mode) {
         case WAVE_FUNCTION:
             v = wave_sum(uv);
+            rgb = value_to_rgb(v, find_max_amp());
             break;
         case AMPLITUDE:
             v = amplitude_sum(uv);
+            rgb = value_to_rgb(v, find_max_amp());
+            break;
+        case RELATIVE_PHASE:
+            rgb = theta2rgb(phase_sum(uv));
+            break;
+        case INSTANTANEOUS_PHASE:
+            rgb = theta2rgb(instant_phase_sum(uv));
             break;
     }
-    gl_FragColor = vec4(value_to_rgb(v, max), 1.000);
+    gl_FragColor = vec4(rgb, 1.000);
 }
 """.replace('N_SOURCES', str(N_SOURCES))  # .format() breaks the many brackets.
 
@@ -142,7 +263,7 @@ class Model:
                                       (+1, -1), (+1, +1)]
         self.contrast = 0.5  # Uses setter to change uniform.
         self.wavelength = 4  # Uses setter to change uniform.
-        self.mode = 'wave_function'
+        self.mode = 0
 
         self.sources = [SourceControl(self.program, i)
                         for i in range(N_SOURCES)]
@@ -174,10 +295,16 @@ class Model:
         return self.program['u_mode']
 
     @mode.setter
-    def mode(self, mode: str):
-        if mode not in ('amplitude', 'wave_function'):
+    def mode(self, mode: str or int):
+        if isinstance(mode, int):
+            index = mode
+        elif isinstance(mode, str):
+            if mode not in MODES:
+                raise ValueError('Unexpected mode: {}'.format(mode))
+            index = MODES[mode]
+        else:
             raise ValueError('Unexpected input: {}'.format(mode))
-        self.program['u_mode'] = MODES[mode]
+        self.program['u_mode'] = index
 
 
 class SourceControl:
